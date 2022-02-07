35bad1c6759eeb1c22b467b7f99a547d4e9126fd
epitaq <somaok2004@gmail.com>
2022-02-07 18:18:25 +0900

    背景画像探し

--- multi.py
+++ multi.py
@@ -1,4 +1,5 @@
 import cv2
+from matplotlib import scale
 #import mediapipe as mp
 import numpy as np
 import time
@@ -8,13 +9,17 @@ from hand import hand_point
 from face_mask import face_point
 
 
+
 def movie ():
+    time0 = time.time()
     # カメラCh.(ここでは0)を指定
     camera = cv2.VideoCapture(0)
     #  元となる画像
-
+    ret, img = camera.read()
+    originate_img = np.zeros_like(img)
+    upload_img = cv2.imread('random1.png')
+    originate_img = cv2.resize(upload_img, dsize=(img.shape[1], img.shape[0]))
     # 撮影＝ループ中にフレームを1枚ずつ取得（qキーで撮影終了）
-    time0 = time.time()
     while True:
         #  フレームレートの取得
         time1 = time.time()
@@ -22,22 +27,20 @@ def movie ():
         time0 = time.time()
         # フレームを取得
         ret, img = camera.read()
-        multi_img = np.zeros_like(img)
+        #main_img = img
+        main_img = originate_img
         #  合成
-        # #  eye
-        # eye_result = eye_point(img)
-        # eye_and = cv2.bitwise_and(multi_img, eye_result[0])
-        # multi_img = cv2.add(eye_and, eye_result[1])
+        #  eye faceに合成
         #  face
         face_result = face_point(img)
-        face_and = cv2.bitwise_and(multi_img, face_result[0])
-        multi_img = cv2.add(face_and, face_result[1])
+        face_and = cv2.bitwise_and(main_img, face_result[0])
+        main_img = cv2.add(face_and, face_result[1])
         #  hand
         hand_result = hand_point(img)
-        hand_and = cv2.bitwise_and(multi_img, hand_result[0])
-        multi_img = cv2.add(hand_and, hand_result[1])
+        hand_and = cv2.bitwise_and(main_img, hand_result[0])
+        main_img = cv2.add(hand_and, hand_result[1])
         #  fpsの表示
-        fps_img = cv2.putText(multi_img, text=str(fps)+'fps', org=(0,30), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.5, color=(10,255,10))
+        fps_img = cv2.putText(main_img, text=str(fps)+'fps', org=(0,30), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.5, color=(10,255,10))
         #img = cv2.bitwise_or(img, fps_img)
         img = fps_img
         # フレームを画面に表示
